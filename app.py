26876df6e0c7b8db07159529e2965de4db296738
lul_prof <israelmutua539@gmail.com>
2025-05-08 13:33:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -167,4 +167,4 @@ def internal_error(error):
     return render_template('index.html', projects=get_fallback_projects()), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
